ddeac1db658332deea4348fc8aa96c815b9cc239
hckdnl 0x <heck.daniel94@gmail.com>
2024-06-27 15:19:02 +0000

    Ensure all project files are staged and committed

---
+++ ert_data.sh queries.sh worldcup.sql
@@ -0,0 +1,611 @@
+[33mcommit dc0a142d1d569159534b18d26d263208e02c5a5e[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Author: hckdnl 0x <heck.daniel94@gmail.com>
+Date:   Thu Jun 27 15:13:51 2024 +0000
+
+    fixed
+
+[33mcommit f36171f092f5f404df33951c672aa7240220fbe7[m
+Author: hckdnl 0x <heck.daniel94@gmail.com>
+Date:   Thu Jun 27 15:09:58 2024 +0000
+
+    Fix issues and update project files
+
+[33mcommit 615b5e83ff68e8eb3719ac531c7fbf73964747f5[m
+Author: hckdnl 0x <heck.daniel94@gmail.com>
+Date:   Thu Jun 27 15:09:06 2024 +0000
+
+    Save progress: 1.1
+
+[33mcommit ecd451e6d79345ee1d40f7919c508d11282b9464[m
+Author: hckdnl 0x <heck.daniel94@gmail.com>
+Date:   Thu Jun 27 15:00:04 2024 +0000
+
+    Commit all local changes before pulling
+
+[33mcommit 135f8305d027912e7406747d2b5c2d1879f4da19[m
+Author: hckdnl 0x <heck.daniel94@gmail.com>
+Date:   Thu Jun 27 14:56:02 2024 +0000
+
+    Committing local changes before pulling
+
+[33mcommit 8552cb4c5e815c9ae02865a33169718ec5caf0a7[m
+Author: hckdnl 0x <heck.daniel94@gmail.com>
+Date:   Thu Jun 27 14:53:22 2024 +0000
+
+    Committing local changes before pulling
+
+[33mcommit 8f7f6f0673e08daf23a783b908b02f7b166c821c[m
+Author: hckdnl 0x <heck.daniel94@gmail.com>
+Date:   Thu Jun 27 14:41:59 2024 +0000
+
+    Add worldcup project files
+
+[33mcommit ebe260d95453d6039ee9d37982c0f7ca7af129ec[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Fri Oct 23 12:19:47 2020 -0500
+
+    1.1
+
+[33mcommit 6e4552c6cb0352961d7b248161f65a59aa3cc4a8[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Fri Oct 23 12:18:02 2020 -0500
+
+    INIT
+
+[33mcommit 1e4f01d4ccfff1d08234777e92dcc73f0215dc0e[m
+Author: Tom <20648924+moT01@users.noreply.github.com>
+Date:   Mon Feb 12 20:37:56 2024 -0600
+
+    feat: release v2.0.0 (#27)
+    
+    * feat: v2.0.0
+    
+    * fix: config
+    
+    * chore: update json
+    
+    * fix: config
+    
+    * chore: update json
+    
+    * fix: config
+    
+    * chore: update json
+    
+    * chore: update json
+    
+    * fix: config
+    
+    * fix: config
+    
+    * fix: repo uri
+    
+    * chore: update json
+
+[33mcommit 526758852f4b352622b30e032e4f3f1cc9a91002[m
+Author: Tom <20648924+moT01@users.noreply.github.com>
+Date:   Fri Dec 23 10:10:46 2022 -0600
+
+    fix: clarify user stories (#26)
+    
+    * fix: clarify user stories
+    
+    * update json
+
+[33mcommit 5cc76c29b423eb5c9904c0329bf2af99e5706646[m
+Author: Zaira <33151350+zairahira@users.noreply.github.com>
+Date:   Wed Jun 29 21:04:53 2022 +0500
+
+    fix: improve instruction (#25)
+    
+    * fix: filename corrected to match the actual file
+    
+    * Update json
+    
+    * Update json
+    
+    * Update json
+    
+    * update json
+    
+    * update json
+    
+    * update json
+    
+    * Update tutorial.json
+    
+    Co-authored-by: Tom <20648924+moT01@users.noreply.github.com>
+
+[33mcommit 53213c72b940d847b3fb1953d24bcb0e0d6b3f40[m
+Author: Zaira <33151350+zairahira@users.noreply.github.com>
+Date:   Thu Jun 16 16:59:27 2022 +0500
+
+    fix: Json updated (#24)
+    
+    * fix: filename corrected to match the actual file
+    
+    * update json
+
+[33mcommit 79ebcf256176cf85eae0435dfdfa08ebb4d877be[m
+Author: Tom <20648924+moT01@users.noreply.github.com>
+Date:   Fri Apr 29 15:09:19 2022 -0500
+
+    feat: release v1.0.4 (#22)
+    
+    * feat: release v1.0.4
+    
+    * update json
+
+[33mcommit 0f6d122480a3e13caec38daeddc1ee11c7a66348[m
+Author: tculbreath <44753508+tculbreath@users.noreply.github.com>
+Date:   Sat Apr 23 05:44:26 2022 -0500
+
+    fix: opponent typo (#21)
+    
+    * Add files via upload
+    
+    Changed opponent misspelling for json and md files
+    
+    * Delete learn-world-cup-database-main directory
+    
+    * Updating Typo
+
+[33mcommit c9268d168fc582fdcb5a368ee56205d81fb0af48[m
+Author: Tom <20648924+moT01@users.noreply.github.com>
+Date:   Fri Mar 18 11:25:52 2022 -0500
+
+    Revert "test: webhook on dev (#17)" (#18)
+    
+    This reverts commit ad2320fa8f94dac9e3af8007c0ff37188e8c9627.
+
+[33mcommit bc39c59f1d1091bb9e2a0179c1b37e4499c97f66[m
+Author: Tom <20648924+moT01@users.noreply.github.com>
+Date:   Fri Mar 18 11:04:15 2022 -0500
+
+    test: webhook on dev (#17)
+    
+    * test: webhook on .dev
+    
+    * update json
+
+[33mcommit b0f94d2187b5334651069bdbd870c4611ab6f87b[m
+Author: Tom <20648924+moT01@users.noreply.github.com>
+Date:   Mon Feb 28 14:47:22 2022 -0600
+
+    fix: update webhook to .org and improve instructions (#16)
+    
+    * fix: update webhook
+    
+    * fix: improve instructions
+    
+    * update json
+
+[33mcommit 09798bb081bd50fa7f991d7d28ea8d0bd1d8701f[m
+Author: Tom <20648924+moT01@users.noreply.github.com>
+Date:   Fri Feb 18 16:26:12 2022 -0600
+
+    fix: webhook for .dev (#15)
+    
+    * fix: webhook for .dev
+    
+    * update json
+
+[33mcommit 39c1faa5df21c37531d9b5dbf3993b3c36137e13[m
+Author: Tom <20648924+moT01@users.noreply.github.com>
+Date:   Fri Jan 21 10:30:27 2022 -0600
+
+    feat: release v1.0.3 (#14)
+    
+    * feat: release v1.0.3
+    
+    * update json
+
+[33mcommit 1105d7734ebcce5fe34bdff04ad6685e1e15d2b2[m
+Author: Tom <20648924+moT01@users.noreply.github.com>
+Date:   Wed Jan 19 17:56:49 2022 -0600
+
+    feat: add id to yaml (#13)
+    
+    * feat: add id to yaml
+    
+    * update json
+
+[33mcommit 822798a69e9498a1f9e2c40bed0946a3430ef614[m
+Author: Tom <20648924+moT01@users.noreply.github.com>
+Date:   Wed Dec 8 16:22:24 2021 -0600
+
+    feat: add webhook (#12)
+    
+    * feat: add webhook
+    
+    * update json
+
+[33mcommit 5c7c611f8d17e09755f05fb5579f434e180b6602[m
+Author: Tom <20648924+moT01@users.noreply.github.com>
+Date:   Wed Nov 3 21:07:37 2021 -0500
+
+    fix: typos and simplify grammar in instructions (#11)
+    
+    * fix: typos and rephrase instructions
+    
+    * updated tutorial.json
+    
+    * fix: typos and rephrase instructions
+    
+    * updated tutorial.json
+    
+    * fix: some things and simplify - too much text
+    
+    * update json
+    
+    Co-authored-by: Rafael Hernandez <rafaeldavish@Rafaels-MacBook-Pro.local>
+
+[33mcommit bed6563ccac16d54c32695a29139b6d153c3f6ae[m
+Author: Tom <20648924+moT01@users.noreply.github.com>
+Date:   Wed Nov 3 17:01:10 2021 -0500
+
+    Release: v1.0.2 (#10)
+    
+    * fix: release version 1.0.2 - fix number of unique winning teams
+    
+    * Update json
+    
+    * Modify tutorial to include new teams
+    
+    * Update json
+    
+    * fix: instuctions to 20 second limit
+    
+    * update json
+    
+    * Update changelog for v1.0.2
+    
+    * Update changelog for v1.0.2
+    
+    * Update changelog for v1.0.2
+    
+    * Update json
+    
+    * Update changelog for v1.0.2
+    
+    * Update json
+    
+    * revert: uri back to freeCodeCamp
+    
+    * update json
+    
+    * chore: update changelog
+    
+    Co-authored-by: Ian Cowan <ian@cowanemail.com>
+
+[33mcommit 24a484b0ba878d2d0098c70b148c29976c3c15de[m
+Author: Mrugesh Mohapatra <1884376+raisedadead@users.noreply.github.com>
+Date:   Tue Nov 2 17:13:39 2021 +0530
+
+    Update CHANGELOG.md
+
+[33mcommit c5b7b59fc2cdaae0931fe555bd0af931b315edd9[m
+Author: Ilenia <nethleen@gmail.com>
+Date:   Tue Nov 2 12:42:17 2021 +0100
+
+    feat: update changelog with link to contributing guidelines (#8)
+
+[33mcommit 0a169345c8c62252a1f9603e14ea49aa4bed157f[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Sun Sep 12 10:38:27 2021 -0500
+
+    update json
+
+[33mcommit ce5ebea79865f0ffce59032cb67a959ab47b545c[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Sun Sep 12 10:38:15 2021 -0500
+
+    release: v1.0.1
+
+[33mcommit e85f27c3f86f28e21e0fed9335cc4feeefbf554c[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Fri Sep 10 15:08:43 2021 -0500
+
+    update json
+
+[33mcommit 44b60bf4c06336c0da2ac3b43f8f7ca152e07f52[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Fri Sep 10 15:08:32 2021 -0500
+
+    v1.0.0 - initial release
+
+[33mcommit 2c2a83b7f7fa312227c4302a84528afdca084843[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Aug 12 13:57:21 2021 -0500
+
+    update json
+
+[33mcommit 68d04d8c723357cee3c2c1ce557ecc23df27aa8b[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Aug 12 13:57:11 2021 -0500
+
+    adjust startup commands
+
+[33mcommit 96e71bfbc0b9a4c20ddb4d8bb859d906a6ff1a00[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Jul 8 17:31:28 2021 -0500
+
+    strove to codeally
+
+[33mcommit e192727cc82b9b397f4d6a490c1675817fd35cba[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Sat Jun 5 11:18:11 2021 -0500
+
+    update json
+
+[33mcommit 1030d95f148cb2c5c082fb5df5b8e5a239f423dc[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Sat Jun 5 10:47:23 2021 -0500
+
+    fix: refine instructions
+
+[33mcommit a97b326f77d4ff57f7860903e10a397997cd19f5[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Jun 3 10:59:23 2021 -0500
+
+    update json after updating vscode settings
+
+[33mcommit 6a04388c69eb930b5a6404c7e5859ad383d4cb02[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Jun 3 09:59:03 2021 -0500
+
+    chore: add disable run on save
+
+[33mcommit 5f1577bf75826aa532aaaa512d9e2bcd83f79db9[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Mon Apr 5 14:25:43 2021 -0500
+
+    use main branch in strove.json
+
+[33mcommit f557df5873e894ed3bdd255bb9d137a186d429dc[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Fri Oct 30 09:06:42 2020 -0500
+
+    update json
+
+[33mcommit 47b3637c75566a7d7be24ad6e3b039f375cc53e9[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Fri Oct 30 09:06:22 2020 -0500
+
+    update user stories
+
+[33mcommit efe38ddaa427b6d2883e34b5b560fbb62771a1eb[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Fri Oct 30 08:41:55 2020 -0500
+
+    update json
+
+[33mcommit 3782e8c6cc3f1d9a6cc8eddfee9ebe02649b53c7[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Fri Oct 30 08:41:12 2020 -0500
+
+    update
+
+[33mcommit ecbd65663f3efda8396140568d455a38dacc241f[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Fri Oct 30 08:23:14 2020 -0500
+
+    update json
+
+[33mcommit 04312191688b4d9bbddcbc88819442d8ff2ab8ee[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Fri Oct 30 08:18:34 2020 -0500
+
+    update json
+
+[33mcommit 923f936a8741269f0b505b8da5e208f8d283ea97[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Fri Oct 30 08:11:24 2020 -0500
+
+    update json
+
+[33mcommit bd9a847f5809413b2ce1f32bd203413db8382ddd[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Fri Oct 30 07:54:20 2020 -0500
+
+    update json
+
+[33mcommit 975a9ad82fe52f586162eae03b84f50e2a92862e[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 20:22:35 2020 -0500
+
+    add write protection to expected output file
+
+[33mcommit b414d4ede0793b05fd98179f1723e731244ac867[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 20:21:48 2020 -0500
+
+    add write protection to csv
+
+[33mcommit bfa32d85118f24658cb0074131a7d1d1280101e6[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 15:57:45 2020 -0500
+
+    update
+
+[33mcommit 0df8e1acf1a62758b4bfd7e8ae6b016f0f49bdee[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 15:45:58 2020 -0500
+
+    update
+
+[33mcommit 87635def1fd5a1b0a173cddce5bbb2890079b019[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 15:43:52 2020 -0500
+
+    revert read only setting - doesn't seem to work
+
+[33mcommit b07d9c37c2a74ca57f4e5142e023a492d91fbc69[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 15:39:33 2020 -0500
+
+    update json
+
+[33mcommit 65ed5a19073c3e67186f45db2a04c16d356c8e31[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 15:39:21 2020 -0500
+
+    make csv file read only
+
+[33mcommit d661c96525dbc601590aa5b7951fc8b60ea5d5bd[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 15:34:21 2020 -0500
+
+    update json
+
+[33mcommit 6ae1929082fecb97f287289c950f013b5acd210b[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 13:43:44 2020 -0500
+
+    update user stories
+
+[33mcommit 7d6882dc3ed14ccbac26ec4d7d250d2efde49081[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 13:17:50 2020 -0500
+
+    add note
+
+[33mcommit 44317fb26df61b3ed28b11acaeff5a090c29e336[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 12:58:53 2020 -0500
+
+    update json
+
+[33mcommit 90a34be334229c123ed34dd178fe5abda7c238d7[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 12:49:56 2020 -0500
+
+    update json
+
+[33mcommit 65723cbfb813725876ff9ba796af36ea4d0797c1[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 12:48:40 2020 -0500
+
+    update user stories
+
+[33mcommit 6aab8295dba804365f743ce3c2165678a1504f7c[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 12:33:43 2020 -0500
+
+    update json
+
+[33mcommit a2922e487c3eeb618a5f362faed8d0de8b507b79[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 12:31:15 2020 -0500
+
+    update user stories
+
+[33mcommit 02230956ea1a62fcfbe5edbcca2a5182ca28adf5[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 12:19:41 2020 -0500
+
+    update
+
+[33mcommit 66d169b204b3ca624d2944327f18d9bd725ea54e[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 12:10:01 2020 -0500
+
+    test
+
+[33mcommit f7ac241fcdb97092327202f7bf562e468280e4c0[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 12:09:49 2020 -0500
+
+    test
+
+[33mcommit 5ee46d96316dc4e538575438e4599dc3b7a55782[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 12:06:22 2020 -0500
+
+    test
+
+[33mcommit 0e75b8822bdb9e547e481964e74d3c5cdd2f0857[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 12:06:10 2020 -0500
+
+    test
+
+[33mcommit 6bc65ae199de123640df63d6aba69eb71fcdd20c[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 12:04:38 2020 -0500
+
+    test
+
+[33mcommit 6ab9679d2f53468b8f2f01a48f7c82cf579cc3c6[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 12:04:11 2020 -0500
+
+    test
+
+[33mcommit f022976efed5455bb771ceb63240013c8196ced6[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 12:03:08 2020 -0500
+
+    update json
+
+[33mcommit 9bd67ef6d6e5fb50dbef7dbeddea7f41ec1a1851[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 12:02:56 2020 -0500
+
+    update
+
+[33mcommit 0e8e1a746a37ad8ea2df862c1dfa18aa928dbcbf[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 11:55:58 2020 -0500
+
+    update json
+
+[33mcommit 663fd514064bbe20fe4d9b24596b4850102694f8[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 11:27:47 2020 -0500
+
+    update json
+
+[33mcommit a1241b770cf721298db1d8421b7cd8c36b88573a[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 11:27:23 2020 -0500
+
+    try new command
+
+[33mcommit 4687b256fd04efb97389ef1f82a9363883df861c[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 11:24:07 2020 -0500
+
+    update json
+
+[33mcommit 6c19f3190532ffb7e1738501d6a7735c8c79d6c5[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 11:23:54 2020 -0500
+
+    update yaml v0.1.1
+
+[33mcommit f88064e45d871304f096203d405331eded6782a4[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 11:22:38 2020 -0500
+
+    update json v0.1.1
+
+[33mcommit 1a8d2b8d9cdd964a6defbdb5b98536b63a4f03de[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Oct 29 10:57:07 2020 -0500
+
+    update
+
+[33mcommit f8bfcfebd3b1b2bd44118366b6dc8952f20e6823[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Wed Oct 28 13:34:11 2020 -0500
+
+    update json
+
+[33mcommit 0ce5bfe715ac8e2b27f0789c0d4ea7aa643ed5bd[m
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Wed Oct 28 13:30:35 2020 -0500
+
+    update
+
+[33mcommit 4a6a5ae8496eac4ff6b4b0b5cc6828360dbc
